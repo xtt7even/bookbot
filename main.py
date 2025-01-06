c23430459fca5a8d6bff16afe36628d61dc081e7
Kirill Grabar <kirill.grabar4@gmail.com>
2025-01-06 17:58:48 +0100

    Added good looking output

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ with open("books/frankenstein.txt") as f:
     file_contents = f.read()
 
 def main():
-    count = count_words(file_contents)
-
-    count_characters_in_string(file_contents)
+    awesome_aah_output(count_characters_in_string(file_contents), count_words(file_contents))
 
 def count_words(text): 
     splitted_string = text.split()
@@ -17,11 +15,16 @@ def count_characters_in_string(string):
     for symbol in straight_string:
         if symbol not in unique_symbols_arr:
             unique_symbols_arr.append(symbol)
-    print(unique_symbols_arr)
 
     symbols_dict = {}
     for symbol in unique_symbols_arr:
         symbols_dict[symbol] = straight_string.count(symbol)
-    print(symbols_dict)      
+    return symbols_dict
 
+def awesome_aah_output(symbols_dict, word_count):
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+    for symbol, count in symbols_dict.items():
+        print(f"The {symbol} character was found {count} times")
+    print("--- End report ---")
 main()
