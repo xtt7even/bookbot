cd068a165c9fbd25ad31055233894de695feac37
Kirill Grabar <kirill.grabar4@gmail.com>
2025-01-06 17:29:24 +0100

    Add character counting functionality to main.py

--- main.py
+++ main.py
@@ -3,10 +3,25 @@ with open("books/frankenstein.txt") as f:
 
 def main():
     count = count_words(file_contents)
-    print(count)
 
-def count_words(string): 
-    splitted_string = string.split()
+    count_characters_in_string(file_contents)
+
+def count_words(text): 
+    splitted_string = text.split()
     return len(splitted_string)
+
+def count_characters_in_string(string):
+    lowered_string = string.lower()
+    straight_string = lowered_string.replace(" ", "")
+    unique_symbols_arr = []
+    for symbol in straight_string:
+        if symbol not in unique_symbols_arr:
+            unique_symbols_arr.append(symbol)
+    print(unique_symbols_arr)
+
+    symbols_dict = {}
+    for symbol in unique_symbols_arr:
+        symbols_dict[symbol] = straight_string.count(symbol)
+    print(symbols_dict)      
 
 main()
